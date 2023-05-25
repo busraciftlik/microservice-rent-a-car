ed7a1a43f3414da3588d603efde512b6829ed708
Busra Ciftlik <busraciftlik96@gmail.com>
2023-05-25 23:32:44 +0300

    update application.yml

--- inventory-service/src/main/java/com/busraciftlik/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/busraciftlik/inventoryservice/InventoryServiceApplication.java
@@ -3,7 +3,9 @@ package com.busraciftlik.inventoryservice;
 import com.busraciftlik.common.util.constants.Paths;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = {Paths.ConfigurationBasePackage,Paths.Inventory.ServiceBasePackage})
 public class InventoryServiceApplication {
 
